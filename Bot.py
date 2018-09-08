6d4795df8255c1d85ab094d95b91b64a815aa0d1
YLWS-4617 <yuluowusheng4617@gmail.com>
2018-09-08 16:16:25 -0400

    新增骚话功能

--- Bot.py
+++ Bot.py
@@ -13,14 +13,22 @@ Data = file.read().strip().split("\n")
 
 bot = telebot.TeleBot(TOKEN)
 
-def yiyan(message):
+def yiyan():
 	Index=random.randint(0,len(Data)-1)
 	print("Log: " + "一言: " + Data[Index])
-	bot.send_message(reply_to_message_id=message.message_id, chat_id=message.chat.id, text=Data[Index],reply_markup=markup)
+	return Data[Index]
 
 @bot.message_handler(commands=['yiyan'])
 def send_welcome(message):
-	yiyan(message);
+	bot.send_message(reply_to_message_id=message.message_id, chat_id=message.chat.id, text=yiyan(),reply_markup=markup)
+
+@bot.message_handler(commands=['sao'])
+def sao(message):
+	Msg = ""
+	for x in range(0,random.randint(3,5)):
+		Msg = Msg + yiyan() + " "
+	bot.send_message(reply_to_message_id=message.message_id, chat_id=message.chat.id, text=Msg,reply_markup=markup)
+
 
 if __name__ == '__main__':
 	bot.polling()
